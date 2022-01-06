bfe2cfc77766ad1dd3b9356744b524f3e2d4f39d
Chris Chamberlain <chrisbc@artisan.co.nz>
2022-01-06 13:39:38 +1300

    added dynamodb support for solutoin analyses

--- api/api.py
+++ api/api.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from .namespaces import api
 
+from api.model import set_local_mode, migrate
+
 app = Flask(__name__)
+# app.config.from_object(Config)
+
 api.init_app(app)
 
+
+set_local_mode() #make this pick up the config
+app.before_first_request(migrate)
+
 if __name__ == '__main__':
+    # Register table creation callback
+
     app.run()
--- api/namespaces/solutions.py
+++ api/namespaces/solutions.py
@@ -1,4 +1,5 @@
 #!python
+from uuid import uuid4
 
 from flask_restx import Namespace, Resource, fields
 from api.toshi_api.toshi_api import ToshiApi
@@ -16,15 +17,18 @@ api = Namespace('solutions', description='Solution analysis related operations')
 headers={"x-api-key":API_KEY}
 toshi_api = ToshiApi(API_URL, S3_URL, None, with_schema_validation=False, headers=headers)
 
+from api.model import SolutionLocationsRadiiDF
+
+
+#need to keep this in sync with pynamodb model.SolutionLocationsRadiiDF. Must be a better way
 solution_analysis_model = api.model('Solution Analysis', dict(
+    id = fields.String(description='The unique identifier for this analysis'),
     solution_id = fields.String(required=True, description='The solution identifier (ref Toshi API '),
     locations_list_id = fields.String(required=True, description='The locations_list ID (see /location_lists)'),
     radii_list_id = fields.String(required=True, description='The radii_list ID (see /radii_lists)'),
+    created = fields.DateTime(description='The created timestamp'),
+    dataframe = fields.String(description='The dataframe in JSON, only availble with filter or single get')
 ))
-
-SOLUTION_ANALYSES = [
-    dict(solution_id='SW52ZXJzaW9uU29sdXRpb246MTc1NTQuMDdzQ3d1', locations_list_id="NZ", radii_list_id="1"),
-]
 
 @api.route('/')
 class SolutionAnalysisList(Resource):
@@ -32,28 +36,21 @@ class SolutionAnalysisList(Resource):
     @api.marshal_list_with(solution_analysis_model)
     def get(self):
         '''List all solution'''
-        return SOLUTION_ANALYSES
-
-@api.route('/<solution_id>/<locations_list_id>/<radii_list_id>')
-@api.param('solution_id', 'The solution analysis identifier')
-@api.response(404, 'solution analysis not found')
-class SolutionAnalysis(Resource):
-    @api.doc('get_solution_analysis ')
-    @api.marshal_with(solution_analysis_model)
-    def get(self, solution_id, locations_list_id, radii_list_id):
-        '''Fetch a solution_analysis_model given its identifiers'''
-        for loc in SOLUTION_ANALYSES:
-            if loc['solution_id'] == solution_id and \
-                 loc['locations_list_id'] == locations_list_id and \
-                 loc['radii_list_id'] == radii_list_id:
-                return loc
-        api.abort(404)
+        return [x for x in SolutionLocationsRadiiDF.scan(
+            attributes_to_get=["id", "solution_id", "locations_list_id", "radii_list_id", "created"]
+            )]
 
     @api.doc('create_solution_analysis ')
     @api.expect(solution_analysis_model)
     @api.marshal_with(solution_analysis_model)
     @api.response(404, 'solution id not found')
-    def post(self, solution_id, locations_list_id, radii_list_id):
+    def post(self):
+        '''Create a solution_analysis, returning the dataframe'''
+        print("payload", api.payload)
+
+        solution_id = api.payload['solution_id']
+        locations_list_id = api.payload['locations_list_id']
+        radii_list_id = api.payload['radii_list_id']
 
         WORK_PATH = "./tmp"
         #fetch the solution
@@ -64,26 +61,26 @@ class SolutionAnalysis(Resource):
         cities = dict(
             WLG = ["Wellington", -41.276825, 174.777969, 2e5],
             GIS = ["Gisborne", -38.662334, 178.017654, 5e4],
-            CHC = ["Christchurch", -43.525650, 172.639847, 3e5],
-            IVC = ["Invercargill", -46.413056, 168.3475, 8e4],
-            DUD = ["Dunedin", -45.8740984, 170.5035755, 1e5],
-            NPE = ["Napier", -39.4902099, 176.917839, 8e4],
-            NPL = ["New Plymouth", -39.0579941, 174.0806474, 8e4],
-            PMR = ["Palmerston North", -40.356317, 175.6112388, 7e4],
-            NSN = ["Nelson", -41.2710849, 173.2836756, 8e4],
-            BHE = ["Blenheim", -41.5118691, 173.9545856, 5e4],
-            WHK = ["Whakatane", -37.9519223, 176.9945977, 5e4],
-            GMN = ["Greymouth", -42.4499469, 171.2079875, 3e4],
-            ZQN = ["Queenstown", -45.03, 168.66, 15e3],
-            AKL = ["Auckland", -36.848461, 174.763336, 2e6],
-            ROT = ["Rotorua", -38.1446, 176.2378, 77e3],
-            TUO = ["Taupo", -38.6843, 176.0704, 26e3],
-            WRE = ["Whangarei", -35.7275, 174.3166, 55e3],
-            LVN = ["Levin", -40.6218, 175.2866, 19e3],
-            TMZ = ["Tauranga", -37.6870, 176.1654, 130e3],
-            TIU = ['Timaru', -44.3904, 171.2373, 28e3],
-            OAM = ["Oamaru", -45.0966, 170.9714, 14e3],
-            PUK = ["Pukekohe", -37.2004, 174.9010, 27e3],
+            # CHC = ["Christchurch", -43.525650, 172.639847, 3e5],
+            # IVC = ["Invercargill", -46.413056, 168.3475, 8e4],
+            # DUD = ["Dunedin", -45.8740984, 170.5035755, 1e5],
+            # NPE = ["Napier", -39.4902099, 176.917839, 8e4],
+            # NPL = ["New Plymouth", -39.0579941, 174.0806474, 8e4],
+            # PMR = ["Palmerston North", -40.356317, 175.6112388, 7e4],
+            # NSN = ["Nelson", -41.2710849, 173.2836756, 8e4],
+            # BHE = ["Blenheim", -41.5118691, 173.9545856, 5e4],
+            # WHK = ["Whakatane", -37.9519223, 176.9945977, 5e4],
+            # GMN = ["Greymouth", -42.4499469, 171.2079875, 3e4],
+            # ZQN = ["Queenstown", -45.03, 168.66, 15e3],
+            # AKL = ["Auckland", -36.848461, 174.763336, 2e6],
+            # ROT = ["Rotorua", -38.1446, 176.2378, 77e3],
+            # TUO = ["Taupo", -38.6843, 176.0704, 26e3],
+            # WRE = ["Whangarei", -35.7275, 174.3166, 55e3],
+            # LVN = ["Levin", -40.6218, 175.2866, 19e3],
+            # TMZ = ["Tauranga", -37.6870, 176.1654, 130e3],
+            # TIU = ['Timaru', -44.3904, 171.2373, 28e3],
+            # OAM = ["Oamaru", -45.0966, 170.9714, 14e3],
+            # PUK = ["Pukekohe", -37.2004, 174.9010, 27e3],
             HLZ = ["Hamilton", -37.7826, 175.2528, 165e3],
             LYJ = ["Lower Hutt", -41.2127, 174.8997, 112e3]
         )
@@ -91,7 +88,7 @@ class SolutionAnalysis(Resource):
         combos = multi_city_events.city_combinations(cities, 0, 5)
         print(f"city combos: {len(combos)}")
 
-        radii = [10e3,20e3,30e3,40e3,50e3,100e3] #AK could be larger ??
+        radii = [10e3,20e3]#,30e3,40e3,50e3,100e3] #AK could be larger ??
 
         rupture_radius_site_sets = multi_city_events.main(solution, cities, combos, radii)
 
@@ -99,10 +96,33 @@ class SolutionAnalysis(Resource):
         df = pd.DataFrame.from_dict(rupture_radius_site_sets, orient='index')
         df = df.rename(columns=dict(zip(radii, [f'r{int(r/1000)}km' for r in radii])))
 
-        print(df)
+        # print(df)
         ruptures = df.join(solution.ruptures_with_rates)
         print(ruptures)
 
 
         ##Save the dataframe
+        dataframe = SolutionLocationsRadiiDF(
+            id = str(uuid4()),
+            solution_id = solution_id,
+            locations_list_id = locations_list_id,
+            radii_list_id =  radii_list_id,
+            dataframe = ruptures.to_json(indent=2))
+
+        dataframe.save()
+        return dataframe
+
+@api.route('/<id>')
+@api.param('id', 'The solution analysis identifier')
+@api.response(404, 'solution analysis not found')
+class SolutionAnalysis(Resource):
+    @api.doc('get_solution_analysis ')
+    @api.marshal_with(solution_analysis_model)
+    def get(self, id):
+        result = SolutionLocationsRadiiDF.get(id) #     5cd8df26-2370-4bbb-8e3d-03e6453e0c65
+        print( result )
+        if not result:
+            api.abort(404)
+        else:
+            return result
 
